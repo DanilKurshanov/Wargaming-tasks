becb929cecb61e885103bef842d20ea509410cb9
KurshanovDanil <kurshanov.danil@gmail.com>
2022-02-17 04:14:29 +0300

    Задача №2.2: класса реализовывающих циклический буфер FIFO

--- CircularBuffer.py
+++ CircularBuffer.py
@@ -23,3 +23,35 @@ class CircularBufferV1:
         if len(self.data) == 0:
             return None
         return self.data.pop()
+
+
+class CircularBufferV2:
+    # Конструктор класса, реализующий циклический стек
+    # При создании экземпляа класса формируте пустой стек заполенный None
+    def __init__(self, size = 4):
+        self.start = 0 # Начало циклического стека - отображает самый "старый" элемент в стеке
+        self.end = -1 # Конец циклического стека - отображает позицию для записи нового элемента в стек
+        self.data = [None for _ in range(size)]
+        self.size = size
+
+    def push(self, elem):
+        '''
+        B.push(elem) -> добавляет элемент в стек
+        При заполненом стеке удаляет самый старый элемент и заменят новым
+        '''
+        if None not in self.data:
+            self.start = (self.start + 1) % self.size
+
+        self.end = (self.end + 1) % self.size
+        self.data[self.end] = elem
+
+
+    def get(self):
+        '''
+        B.get() -> Возвращает самый "старый" элемент из стека
+        Возвращает None если стек пустой
+        '''
+        removed, self.data[self.start] = self.data[self.start], None
+        self.start = (self.start + 1) % self.size
+
+        return removed
